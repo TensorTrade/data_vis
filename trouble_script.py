96e37621ab84b2fbcd5ad24026e7b37195626009
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-09-14 15:05:11 +0530

    Temporarily disable account thread

--- trouble_script.py
+++ trouble_script.py
@@ -249,9 +249,9 @@ class AccountThread(threading.Thread):
 def main():
     """Main function to handle different activites of the account."""
     streamer = StreamThread(ts)  # For the troubling part.
-    account_manager = AccountThread(t)  # For retweets, likes, follows.
+    #account_manager = AccountThread(t)  # For retweets, likes, follows.
     streamer.start()
-    account_manager.run()
+    #account_manager.run()
 
 # Execute the main() function only if script is executed directly.
 if __name__ == "__main__":
