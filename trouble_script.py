5d07b7aa193ff9a04b97ff4e086b9f69d26a9ea9
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-09-21 22:14:55 +0530

    Lint the code

--- trouble_script.py
+++ trouble_script.py
@@ -46,22 +46,36 @@ with requests.get(links['bads']) as bads_file:
 with requests.get(links['messages']) as messages_file:
     messages = messages_file.text.split('\n')
 
+# Following offensive compilation is not my stuff.
+# Copyright (c) 2013-2017 Molly White.
 offensive = re.compile(
-    r"\b(deaths?|dead(ly)?|die(s|d)?|hurts?|(sex(ual(ly)?)?|child)[ -]?(abused?|trafficking|"
-    r"assault(ed|s)?)|injur(e|i?es|ed|y)|kill(ing|ed|er|s)?s?|wound(ing|ed|s)?|fatal(ly|ity)?|"
-    r"shoo?t(s|ing|er)?s?|crash(es|ed|ing)?|attack(s|ers?|ing|ed)?|murder(s|er|ed|ing)?s?|"
-    r"hostages?|(gang)?rap(e|es|ed|ist|ists|ing)|assault(s|ed)?|pile-?ups?|massacre(s|d)?|"
-    r"assassinate(d|s)?|sla(y|in|yed|ys|ying|yings)|victims?|tortur(e|ed|ing|es)|"
-    r"execut(e|ion|ed|ioner)s?|gun(man|men|ned)|suicid(e|al|es)|bomb(s|ed|ing|ings|er|ers)?|"
-    r"mass[- ]?graves?|bloodshed|state[- ]?of[- ]?emergency|al[- ]?Qaeda|blasts?|violen(t|ce)|"
-    r"lethal|cancer(ous)?|stab(bed|bing|ber)?s?|casualt(y|ies)|sla(y|ying|yer|in)|"
-    r"drown(s|ing|ed|ings)?|bod(y|ies)|kidnap(s|ped|per|pers|ping|pings)?|rampage|beat(ings?|en)|"
-    r"terminal(ly)?|abduct(s|ed|ion)?s?|missing|behead(s|ed|ings?)?|homicid(e|es|al)|"
-    r"burn(s|ed|ing)? alive|decapitated?s?|jihadi?s?t?|hang(ed|ing|s)?|funerals?|traged(y|ies)|"
-    r"autops(y|ies)|child sex|sob(s|bing|bed)?|pa?edophil(e|es|ia)|9(/|-)11|Sept(ember|\.)? 11|"
+    r"\b(deaths?|dead(ly)?|die(s|d)?|hurts?|(sex(ual(ly)?)?|"
+    r"child)[ -]?(abused?|trafficking|"
+    r"assault(ed|s)?)|injur(e|i?es|ed|y)|kill(ing|ed|er|s)?s?|"
+    r"wound(ing|ed|s)?|fatal(ly|ity)?|"
+    r"shoo?t(s|ing|er)?s?|crash(es|ed|ing)?|attack(s|ers?|ing|ed)?|"
+    r"murder(s|er|ed|ing)?s?|"
+    r"hostages?|(gang)?rap(e|es|ed|ist|ists|ing)|assault(s|ed)?|"
+    r"pile-?ups?|massacre(s|d)?|"
+    r"assassinate(d|s)?|sla(y|in|yed|ys|ying|yings)|victims?|"
+    r"tortur(e|ed|ing|es)|"
+    r"execut(e|ion|ed|ioner)s?|gun(man|men|ned)|suicid(e|al|es)|"
+    r"bomb(s|ed|ing|ings|er|ers)?|"
+    r"mass[- ]?graves?|bloodshed|state[- ]?of[- ]?emergency|al[- ]?Qaeda|"
+    r"blasts?|violen(t|ce)|"
+    r"lethal|cancer(ous)?|stab(bed|bing|ber)?s?|casualt(y|ies)|"
+    r"sla(y|ying|yer|in)|"
+    r"drown(s|ing|ed|ings)?|bod(y|ies)|kidnap(s|ped|per|pers|ping|pings)?|"
+    r"rampage|beat(ings?|en)|"
+    r"terminal(ly)?|abduct(s|ed|ion)?s?|missing|behead(s|ed|ings?)?|"
+    r"homicid(e|es|al)|"
+    r"burn(s|ed|ing)? alive|decapitated?s?|jihadi?s?t?|hang(ed|ing|s)?|"
+    r"funerals?|traged(y|ies)|"
+    r"autops(y|ies)|child sex|sob(s|bing|bed)?|pa?edophil(e|es|ia)|"
+    r"9(/|-)11|Sept(ember|\.)? 11|"
     r"genocide)\W?\b",
-    flags=re.IGNORECASE) #Copyright (c) 2013-2016 Molly White
-    #Above offensive compilation is not my stuff
+    flags=re.IGNORECASE)
+
 
 def reply(tweet_id, user_name, msg):
     """
@@ -77,6 +91,7 @@ def reply(tweet_id, user_name, msg):
 
 def print_tweet(tweet):
     """Displays the primary contents of a tweet, maybe except links."""
+
     print(tweet["user"]["name"])
     print(tweet["user"]["screen_name"])
     print(tweet["created_at"])
@@ -92,34 +107,39 @@ def fav_tweet(tweet):
     """Favorites a passed tweet and returns a success status - 1 if successful
     otherwise 0.
     """
-     try:
-             result = t.favorites.create(_id=tweet['id'])
-             return 1
-     except TwitterHTTPError:
-             return 0
+    try:
+        result = t.favorites.create(_id=tweet['id'])
+        return 1
+    except TwitterHTTPError:
+        return 0
 
 
 def retweet(tweet):
     """Retweets a passed tweet and returns a success status - 1 if successful
     otherwise 0.
     """
-     try:
-             t.statuses.retweet._id(_id=tweet["id"])
-             return 1
-     except TwitterHTTPError:
-             return 0
+
+    try:
+        t.statuses.retweet._id(_id=tweet["id"])
+        return 1
+
+    except TwitterHTTPError:
+        return 0
 
 
-def quote_tweet(tweet, text): #may not work for long links because of 140-limit. Can be improved.
-     id = tweet["id"]
-     sn = tweet["user"]["screen_name"]
-     link = "https://twitter.com/%s/status/%s" %(sn, id)
-     try:
-             string = text + " " + link
-             t.statuses.update(status=string)
-             return 1
-     except TwitterHTTPError:
-             return 0
+def quote_tweet(tweet, text):
+    """Quotes a passed tweet with a passed text and then tweets it."""
+
+    # May not work for long links because of 140-limit. Can be improved.
+    id = tweet["id"]
+    sn = tweet["user"]["screen_name"]
+    link = "https://twitter.com/%s/status/%s" % (sn, id)
+    try:
+        string = text + " " + link
+        t.statuses.update(status=string)
+        return 1
+    except TwitterHTTPError:
+        return 0
 
 
 def unfollow(iden):
@@ -129,7 +149,6 @@ def unfollow(iden):
         success += 1
     except Exception as e:
         print(e)
-    #print "Unfollowed %i people." % success
 
 
 def shorten_url(url):
@@ -168,9 +187,8 @@ class StreamThread(threading.Thread):
                 also gets unwanted retweets, replies and so on.
                 """
                 if tweet['user']['id'] not in bads:
-                    #print("Ignored from:", tweet['user']['screen_name'])
                     continue
-                
+
                 # Gets messages to tweet.
                 with requests.get(links['messages']) as messages_file:
                     messages = messages_file.text.split('\n')[:-1]
@@ -187,10 +205,17 @@ class StreamThread(threading.Thread):
                         max_length = len(word)
                         max_word = word
                         """
-                
+
                 # Searches for a related tweet, later add images.
-                rep_tweet = t.search.tweets(q=tweet["user"]["name"], count=1, lang="en")["statuses"][0] #understand OR operator
-                tweet_link = "https://twitter.com/"+rep_tweet["user"]["screen_name"]+"/status/"+rep_tweet["id_str"]
+                rep_tweet = t.search.tweets(
+                    q=tweet["user"]["name"],
+                    count=1, lang="en"
+                    )["statuses"][0]
+
+                tweet_link = \
+                    "https://twitter.com/"\
+                    + rep_tweet["user"]["screen_name"]\
+                    + "/status/"+rep_tweet["id_str"]
                 short_url = shorten_url(tweet_link)
                 message = random.choice(messages) + " " + short_url
                 reply(
@@ -198,18 +223,19 @@ class StreamThread(threading.Thread):
                     tweet['user']['screen_name'],
                     message
                 )
-                
+
                 # Print tweet for logging.
                 print('-*-'*33)
                 print_tweet(tweet)
                 print('*'*33)
                 print_tweet(rep_tweet)
                 print('-*-'*33)
-            except Exception as e:  # So that loop doesn't stop if error occurs.
+            except Exception as e:
+                # Loop shouldn't stop if error occurs, and exception should be
+                # logged.
                 print(json.dumps(tweet, indent=4))
                 print(e)
             print('-*-'*33)
-            """You yourself are an embodiment of fake news. <some random link>"""
 
 
 class AccountThread(threading.Thread):
@@ -225,16 +251,28 @@ class AccountThread(threading.Thread):
                 words = keywords_file.text.split('\n')
             word = random.choice(words)
             # Add '-from:TheRealEqualizer' in the following line.
-            tweets = self.t.search.tweets(q=word, count=199, lang="en")["statuses"] #understand OR operator
+            tweets = self.t.search.tweets(
+                q=word, count=199,
+                lang="en"
+                )["statuses"]  # Understand OR operator.
 
             with requests.get(links['screen_name']) as screen_name_file:
                 screen_name = screen_name_file.text.strip()
 
             fr = t.friends.ids(screen_name=screen_name)["ids"]
-            if len(fr) > 4000: #To unfollow old follows because Twitter doesn't allow a large following / followers ratio for people with less followers.
-                               #Using 4000 instead of 5000 for 'safety', so that I'm able to follow some interesting people
-                               #manually even after a bot crash.
-                for i in range(1000): #probably this is the upper limit of mass unfollow in one go
+            if len(fr) > 4000:
+                """
+                To unfollow old follows because Twitter doesn't allow a large
+                following / followers ratio for people with less followers.
+                Using 4000 instead of 5000 for 'safety', so that I'm able to
+                follow some interesting people manually even after a bot
+                crash.
+                """
+
+                """
+                Perhaps 1000 is the upper limit of mass unfollow in one go.
+                """
+                for i in range(1000):
                     unfollow(fr.pop())
 
             for tweet in tweets:
@@ -257,10 +295,12 @@ class AccountThread(threading.Thread):
 
 def main():
     """Main function to handle different activites of the account."""
+
     streamer = StreamThread(ts)  # For the troubling part.
     account_manager = AccountThread(t)  # For retweets, likes, follows.
     streamer.start()
     account_manager.run()
+
 
 # Execute the main() function only if script is executed directly.
 if __name__ == "__main__":
