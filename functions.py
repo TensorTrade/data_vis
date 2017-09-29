42559499443e68eea27d41c1d6ebf962b34924d5
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-09-29 21:58:30 +0530

    Lint functions.py further

--- functions.py
+++ functions.py
@@ -7,13 +7,13 @@ from trouble_script import SHORTE_ST_TOKEN
 from twitter import TwitterHTTPError
 # Here 't' is account handler defined in the main script.
 
-def reply(t, tweet_id, user_name, msg):
+def reply(account_handler, tweet_id, user_name, msg):
     """
     Sends msg as reply to the tweet whose id is passed.
     user_name of the tweet's author is required as per Twitter API docs.
     """
 
-    t.statuses.update(
+    account_handler.statuses.update(
         status='@%s %s' % (user_name, msg),
         in_reply_to_status_id=tweet_id
         )
@@ -26,57 +26,57 @@ def print_tweet(tweet):
     print(tweet["user"]["screen_name"])
     print(tweet["created_at"])
     print(tweet["text"])
-    hashtags = []
-    hs = tweet["entities"]["hashtags"]
-    for h in hs:
-        hashtags.append(h["text"])
-    print(hashtags)
+    hashtags_list = []
+    hashtags = tweet["entities"]["hashtags"]
+    for tag in hashtags:
+        hashtags_list.append(tag["text"])
+    print(hashtags_list)
 
 
-def fav_tweet(t, tweet):
+def fav_tweet(account_handler, tweet):
     """Favorites a passed tweet and returns a success status - 0 if successful
     otherwise 1.
     """
     try:
-        t.favorites.create(_id=tweet['id'])
+        account_handler.favorites.create(_id=tweet['id'])
         return 0
     except TwitterHTTPError:
         return 1
 
 
-def retweet(t, tweet):
+def retweet(account_handler, tweet):
     """Retweets a passed tweet and returns a success status - 0 if successful
     otherwise 1.
     """
 
     try:
-        t.statuses.retweet._id(_id=tweet["id"])
+        account_handler.statuses.retweet._id(_id=tweet["id"])
         return 0
 
     except TwitterHTTPError:
         return 1
 
 
-def quote_tweet(t, tweet, text):
+def quote_tweet(account_handler, tweet, text):
     """Quotes a passed tweet with a passed text and then tweets it."""
 
     # May not work for long links because of 140-limit. Can be improved.
-    id = tweet["id"]
-    sn = tweet["user"]["screen_name"]
-    link = "https://twitter.com/%s/status/%s" % (sn, id)
+    tweet_id = tweet["id"]
+    screen_name = tweet["user"]["screen_name"]
+    link = "https://twitter.com/%s/status/%s" % (screen_name, tweet_id)
     try:
         string = text + " " + link
-        t.statuses.update(status=string)
+        account_handler.statuses.update(status=string)
         return 0
     except TwitterHTTPError:
         return 1
 
 
-def unfollow(t, iden):
+def unfollow(account_handler, iden):
     """Unfollows the person identified by 'iden', returns 0 if successful,
     otherwise 0."""
     try:
-        t.friendships.destroy(_id=iden)
+        account_handler.friendships.destroy(_id=iden)
         return 0
     except TwitterHTTPError:
         return 1
