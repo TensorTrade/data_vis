d6e80dfe53bdcb355b9761bc00e68364abba76d2
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-09-30 23:44:47 +0530

    Lint trouble_script.py

--- trouble_script.py
+++ trouble_script.py
@@ -4,10 +4,9 @@ This script gives people what they deserve on Twitter, and tries luck in 'RT
 to win' competititons.
 """
 import os
-import requests
-import threading
-from managers import *  # Useful classes for streaming and account handling. 
-from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+import managers  # Useful classes for streaming and account handling.
+from twitter import Twitter, OAuth, TwitterStream
 
 # Maybe we can utilize 'RT to win' stuff by this same script.
 # Use the below link for image search.
@@ -15,29 +14,29 @@ from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 # Also post images in replies.
 
 try:
-        oauth = OAuth(
-            os.environ['TW_ACCESS_TOKEN'],
-            os.environ['TW_ACCESS_SECRET'],
-            os.environ['TW_CONSUMER_KEY'],
-            os.environ['TW_CONSUMER_SECRET']
-        )
-        SHORTE_ST_TOKEN = os.environ['SHORTE_ST_TOKEN']
+    OAUTH= OAuth(
+        os.environ['TW_ACCESS_TOKEN'],
+        os.environ['TW_ACCESS_SECRET'],
+        os.environ['TW_CONSUMER_KEY'],
+        os.environ['TW_CONSUMER_SECRET']
+    )
+    SHORTE_ST_TOKEN = os.environ['SHORTE_ST_TOKEN']
 except KeyError:  # For local runs.
     with open('.env', 'r') as secret:
         exec(secret.read())
-        oauth = OAuth(
+        OAUTH = OAuth(
             ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET
         )
 
-t = Twitter(auth=oauth)
-ts = TwitterStream(auth=oauth)
+ACCOUNT_HANDLER = Twitter(auth=OAUTH)
+STREAM_HANDLER = TwitterStream(auth=OAUTH)
 
 
 def main():
     """Main function to handle different activites of the account."""
 
-    streamer = StreamThread(ts, t)  # For the troubling part.
-    account_manager = AccountThread(t)  # For retweets, likes, follows.
+    streamer = managers.StreamThread(STREAM_HANDLER, ACCOUNT_HANDLER)  # For the troubling part.
+    account_manager = managers.AccountThread(ACCOUNT_HANDLER)  # For retweets, likes, follows.
     streamer.start()
     account_manager.run()
 
